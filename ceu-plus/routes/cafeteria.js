2a81e4744d460ccd7b37cfefd138991797696f0e
jsobrinosolis <j.sobrino3@usp.ceu.es>
2022-01-03 10:41:14 +0100

    Aforo random, inicio implementación BD (Sequelize)

--- ceu-plus/routes/cafeteria.js
+++ ceu-plus/routes/cafeteria.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('cafeteria', { title: 'CEU-Cafeteria' });
+    res.render('cafeteria', { title: 'CEU-Cafeteria', aforo: Math.floor(Math.random() * 100) + 1 + '%' });
 });
 
 module.exports = router;

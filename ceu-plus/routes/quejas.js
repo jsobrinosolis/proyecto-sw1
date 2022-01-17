9685e47fe26d44e67f2dd65a1a0fbf60ba9e6201
carlosoler <c.soler13@usp.ceu.es>
2022-01-17 16:34:54 +0100

    Quejas funcional :+1:

--- ceu-plus/routes/quejas.js
+++ ceu-plus/routes/quejas.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const db = require("../models");
-const quejas = require("../controllers/queja.controller");
+const queja = require("../controllers/queja.controller");
 const Queja = db.quejas;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('quejas', { title: 'CEU-Quejas'});
+    //res.render('quejas', { title: 'CEU-Quejas'});
+    Queja.findAll({raw: true}).then(function (result) {
+        console.log(result);
+        res.render('quejas', { title: 'CEU-Quejas', queja: result});
+    })
 });
-router.post('/', quejas.create);
+
+router.post('/', queja.create);
 /*router.post('/', function(req, res){
     req.session.success = 'Queja enviada con éxito';
     res.redirect('/quejas');

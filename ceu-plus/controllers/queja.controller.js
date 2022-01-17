c1e0a5411f8f6580f98cb4ad133bb8c7dd0dff6e
jsobrinosolis <j.sobrino3@usp.ceu.es>
2022-01-17 17:12:18 +0100

    Delete queja implementado

--- ceu-plus/controllers/queja.controller.js
+++ ceu-plus/controllers/queja.controller.js
@@ -35,3 +35,16 @@ exports.create = (req, res) => {
 exports.findUser = (req, res) => {
     Queja.findAll().then()
 }
+
+exports.deleteQueja = (req, res, title) => {
+    Queja.destroy({
+        where: { title: title }
+    })
+        .then(res.redirect('/quejas'))
+        .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || "Some error occurred."
+        });
+})
+}
